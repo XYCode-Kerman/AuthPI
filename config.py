8ce2e705c40cf39cd742bf5f430aacba69ca1408
XYCode Kerman <xycode-xyc@outlook.com>
2024-06-02 23:15:51 +0800

    :construction: config.py新增配置项

--- config.py
+++ config.py
@@ -8,3 +8,7 @@ DATABASE_URL = os.environ.get("DATABASE_URL")
 DATABASE_NAME = os.environ.get("DATABASE_NAME")
 SECRET = os.environ.get("SECRET")
 SUPER_USER_TOKEN = os.environ.get("SUPER_USER_TOKEN")
+ISSUER = os.environ.get("ISSUER")
+
+if ISSUER is None or not ISSUER.startswith('https'):
+    print('ISSUER must be a https URL')
